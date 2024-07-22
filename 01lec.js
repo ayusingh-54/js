f14f0502f9ebe9355d81240ba3d59e8e4ce92202
ayusingh-54 <2023071025@mmmut.ac.in>
2024-07-22 14:21:19 +0530

    Create 01lec.js

---
+++ 01lec.js
@@ -0,0 +1,13 @@
+const accountId =23566
+let accountemail = "ayush692aaaaa2gmail.com"
+var accoutpassword = "1234"
+accountcity ="jaipur"
+let accountstate ;
+// accountId = 7
+accountemail = "adhh@gmail.com"
+accoutpassword = "12463765367"
+accountcity = "udaypur"
+
+console.log([accountId, accountemail, accoutpassword,accountcity, accountstate]);
+// prefer not to use  var
+// because of issue in block scope and functional scope
