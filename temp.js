56756a22ad3e402a6c0412f17776a57f3c622178
Ayush Singh <2023071025@mmmut.ac.in>
2024-07-29 23:13:11 +0530

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+let score = "ffggghh"
+
+
+console.log(typeof score);
+console.log(typeof (score))
+
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber)
+console.log ( valueInNumber)
+ 
+
+
+//"33"=> 33
+//"33wdd"=> NaNA
+//true => 1; false => 0
