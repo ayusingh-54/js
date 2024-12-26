c2ab57e85a28f716bb59e28c1ec70b39545525ed
Ayush Singh <2023071025@mmmut.ac.in>
2024-12-26 15:12:01 +0530

    Add files via upload

---
+++ 0_variables.js
@@ -0,0 +1,26 @@
+// * ====================================
+// * Values and Variable in JavaScript
+// * ====================================
+
+// * In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+
+// * Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+// * Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+// ! Let's test
+// var my_firstName = "John"; // valid variable name
+
+// var _myLastName$ = "Doe"; // valid variable name
+
+// var 123myAge = 25; // invalid variable name
+
+// var $cityName = "New York"; // valid variable name
+
+// var my@Email = "john@example.com"; / invalid variable name
+
+console.log(" hii I Am Ayush Singh ")
+var myFirstName = "Ayush";
+var myLastName = "Singh";
+var myAge = 25;
+console.log(myFirstName, myLastName, myAge);
